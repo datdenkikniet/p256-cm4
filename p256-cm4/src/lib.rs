a1e4c72000a46c9d1c5d800cd0ae441f368a1ffd
datdenkikniet <jcdra1@gmail.com>
2025-10-16 22:51:42 +0200

    Add sec1 decoding

--- p256-cm4/src/lib.rs
+++ p256-cm4/src/lib.rs
@@ -4,10 +4,32 @@
 mod sys;
 pub use sys::*;
 
-use crate::asm::{Montgomery, P256_point_is_on_curve, P256_to_montgomery};
+use crate::asm::{Montgomery, P256_decompress_point, P256_point_is_on_curve, P256_to_montgomery};
 
 #[cfg(target_arch = "arm")]
 pub(crate) mod asm;
+
+#[derive(defmt::Format, Debug, Clone, PartialEq)]
+pub enum VerifyingKeyError {
+    /// Either the `x` or `y` coordinate of the provided point
+    /// were outside of the range `0..=p - 1`.
+    OutOfRange,
+    /// The provided point was not on the `p256` curve.
+    NotOnCurve,
+}
+
+#[derive(defmt::Format, Debug, Clone, PartialEq)]
+pub enum VerifyingKeySec1Error {
+    /// An error occurred with the point that was decoded.
+    Point(VerifyingKeyError),
+    /// The provided amount of data was too small and could
+    /// not be decoded.
+    TooLittleData,
+    /// The provided data had an invalid `sec1` tag.
+    InvalidTag,
+    /// The parity data did not match.
+    InvalidParity,
+}
 
 /// A verifying key, also called a verifying key.
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -18,19 +40,11 @@ pub struct VerifyingKey {
 }
 
 impl VerifyingKey {
-    /// Create a new [`VerifyingKey`] from the provided raw, big-endian encoded
-    /// `x` and `y` coordinates.
+    /// Create a new [`VerifyingKey`] if the point `(x, y)` (where both
+    /// `x` and `y` are in the range `0..=p - 1`) is valid, or return `None`.
     ///
-    /// An error is returned if `x` or `y` is not in the range `0..=p - 1`, where `p`
-    /// is the `p256` prime, or if the point `(x, y)` is not on the `p256` curve.
-    pub fn from_parts(x: &[u8; 32], y: &[u8; 32]) -> Result<Self, ()> {
-        let x = to_little_endian(x);
-        let y = to_little_endian(y);
-
-        if unsafe { !crate::asm::P256_check_range_p(&x) || !crate::asm::P256_check_range_p(&y) } {
-            return Err(());
-        }
-
+    /// In this context, "valid" means that the point `(x, y)` lies on the `p256` curve.
+    fn from_parts_valid_range(x: [u32; 8], y: [u32; 8]) -> Result<Self, VerifyingKeyError> {
         let mut x_mont = Montgomery::default();
         let mut y_mont = Montgomery::default();
 
@@ -39,7 +53,87 @@ impl VerifyingKey {
 
         let valid = unsafe { P256_point_is_on_curve(&x_mont, &y_mont) };
 
-        if valid { Ok(Self { x, y }) } else { Err(()) }
+        if valid {
+            Ok(Self { x, y })
+        } else {
+            Err(VerifyingKeyError::NotOnCurve)
+        }
+    }
+
+    /// Create a new [`VerifyingKey`] from the provided raw, big-endian encoded
+    /// `x` and `y` coordinates.
+    ///
+    /// An error is returned if `x` or `y` is not in the range `0..=p - 1`, where `p`
+    /// is the `p256` prime, or if the point `(x, y)` is not on the `p256` curve.
+    pub fn from_parts(x: &[u8; 32], y: &[u8; 32]) -> Result<Self, VerifyingKeyError> {
+        let x = to_little_endian(x);
+        let y = to_little_endian(y);
+
+        if unsafe { !crate::asm::P256_check_range_p(&x) || !crate::asm::P256_check_range_p(&y) } {
+            return Err(VerifyingKeyError::OutOfRange);
+        }
+
+        Self::from_parts_valid_range(x, y)
+    }
+
+    /// Convert this [`VerifyingKey`] to its [`sec1`] representation (uncompressed).
+    ///
+    /// [`sec1`]: https://docs.rs/sec1/latest/sec1/
+    pub fn to_sec1_bytes(&self) -> [u8; 65] {
+        let mut output = [0u8; 65];
+        output[0] = 4;
+        to_big_endian(&self.x, (&mut output[1..33]).try_into().unwrap());
+        to_big_endian(&self.y, (&mut output[33..65]).try_into().unwrap());
+        output
+    }
+
+    /// Parse the provided `data` as [`sec1`] bytes and return the
+    /// result if it constitutes a valid [`VerifyingKey`] (see
+    /// [`VerifyingKey::from_parts`] for more information on validity).
+    ///
+    /// [`sec1`]: https://docs.rs/sec1/latest/sec1/
+    pub fn from_sec1_bytes(data: &[u8]) -> Result<Self, VerifyingKeySec1Error> {
+        let (tag, xy) = data
+            .split_at_checked(1)
+            .ok_or(VerifyingKeySec1Error::TooLittleData)?;
+
+        let tag = tag[0];
+
+        let (x, y) = xy
+            .split_at_checked(32)
+            .ok_or(VerifyingKeySec1Error::TooLittleData)?;
+
+        let x: &[u8; 32] = x.try_into().unwrap();
+
+        if tag == 4 || tag == 6 || tag == 7 {
+            let y: &[u8; 32] = y
+                .try_into()
+                .map_err(|_| VerifyingKeySec1Error::TooLittleData)?;
+
+            if tag == 6 || tag == 7 {
+                let expected_parity = tag & 0x1;
+                let parity = y[31] & 0x1;
+
+                if expected_parity != parity {
+                    return Err(VerifyingKeySec1Error::InvalidParity);
+                }
+            }
+
+            Self::from_parts(x, y).map_err(VerifyingKeySec1Error::Point)
+        } else if tag == 2 || tag == 3 {
+            let x = to_little_endian(x);
+            let mut y = [0u32; 8];
+
+            let valid = unsafe { P256_decompress_point(&mut y, &x, u32::from(tag) & 0x1) };
+
+            if valid {
+                Self::from_parts_valid_range(x, y).map_err(VerifyingKeySec1Error::Point)
+            } else {
+                Err(VerifyingKeySec1Error::Point(VerifyingKeyError::OutOfRange))
+            }
+        } else {
+            Err(VerifyingKeySec1Error::InvalidTag)
+        }
     }
 
     /// Verify that the private-key counterpart to this [`VerifyingKey`] has produced
@@ -108,4 +202,20 @@ fn to_little_endian(input: &[u8; 32]) -> [u32; 8] {
     }
 
     output
+}
+
+/// Convert an input 256 bit number (little-endian u32s) into the format
+/// used by `sec1` (big-endian u8s)
+fn to_big_endian(input: &[u32; 8], output: &mut [u8; 32]) {
+    let input_ref = transmute(&input);
+    convert_endianness(output, input_ref);
+
+    fn transmute<'a>(input: &'a [u32; 8]) -> &'a [u8; 32] {
+        // SAFETY: we return a mutable reference with the same lifetime
+        // as the input pointer. Additionally, all bit patterns for the
+        // provided `[u32]` are valid for the returned `[u8]`, and the
+        // alignment requirements for an `&mut [u8; 32]` are laxer than
+        // those of a `&mut [u32; 8]`.
+        unsafe { core::mem::transmute(input) }
+    }
 }
